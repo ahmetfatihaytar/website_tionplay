bf9d0fb19f90b967e0497c1eee0efcb93b89a52e
ahmetfatihaytar <ahmetfatihaytar@gmail.com>
2026-05-20 20:19:37 +0300

    Create app-ads.txt.js

---
+++ netlify/functions/app-ads.txt.js
@@ -0,0 +1,9 @@
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+    body: 'google.com, pub-2653290718654152, DIRECT, f08c47fec0942fa0',
+  }
+}
